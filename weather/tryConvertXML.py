b09e8446fef49fa92439d73b52f1bd3a04f5aefb
tamban topayakar <onepiece30233@gmail.com>
2019-01-31 19:21:06 +0700

    decorate program

--- weather/tryConvertXML.py
+++ weather/tryConvertXML.py
@@ -2,17 +2,39 @@ import xmltodict
 import json
 import os
 
-fileName = input("Input File Name (doesn't have to include .xml): ")
+fileName = ""
 
+def fillFileName():
+    global fileName
+    print ("(In this folder, there is weather.xml file alreadyt to test) ")
+    fileName = input("Input File Name (doesn't have to include .xml) : "  )
+
+while(True):
+    try :
+        fillFileName()
+        os.startfile("{0}.xml".format(fileName))
+
+        break
+    except:
+        print("Wrong File Name or File Couldn't be opened")
+        
+    
 with open('{0}.xml'.format(fileName)) as fd:
     doc = dict(xmltodict.parse(fd.read(),xml_attribs=True,attr_prefix = ""))["current"]
 
 
 with open('result.json', 'w') as fp:
      json.dump(doc, fp, indent = 4)
+
+input("----ENTER TO OPEN JSON FILE----")
+
      
 # OPEN THE FILE
-os.startfile("result.json")
+try :
+    os.startfile("result.json")
+    print("CONVERT DONE")
+except :
+    print("There's sth wrong trying to open json file")
 
                  
 
